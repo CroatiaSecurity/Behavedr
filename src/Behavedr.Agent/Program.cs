5a159436e3adbf77da79388eb6a705e4e1c1ff3f
Gorstak <gorstak@users.noreply.github.com>
2026-07-18 19:06:09 +0200

    chore: update

--- src/Behavedr.Agent/Program.cs
+++ src/Behavedr.Agent/Program.cs
@@ -2,6 +2,9 @@ using Behavedr.Core;
 using Behavedr.Windows;
 using Behavedr.Linux;
 using Behavedr.MacOS;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
 
 var engine = new DetectionEngine();
 engine.RegisterMonitor(new WindowsMonitor());
@@ -9,4 +12,22 @@ engine.RegisterMonitor(new LinuxMonitor());
 engine.RegisterMonitor(new MacOSMonitor());
 
 Console.WriteLine("Behavedr Agent started. Behavioral monitoring active.");
-// TODO: tray/systemd loop + response engine
+
+// Windows Tray with your icon
+if (OperatingSystem.IsWindows())
+{
+    var iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Behavedr.ico");
+    if (File.Exists(iconPath))
+    {
+        var tray = new NotifyIcon 
+        { 
+            Icon = new Icon(iconPath), 
+            Visible = true, 
+            Text = "Behavedr EDR - Active" 
+        };
+        // TODO: context menu, double click etc.
+        Console.WriteLine("Tray icon loaded.");
+    }
+}
+
+// TODO: systemd on Linux, full response engine, loop
